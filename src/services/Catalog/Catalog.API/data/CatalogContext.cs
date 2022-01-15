f449aa763e9a840ec6a9d3ba2ea25b4377a40a06
Bahaa Saleh <bahaasaleh2021@hotmail.com>
2022-01-16 00:40:53 +0300

    Catalog.Api Dockerised Done

---
+++ src/services/Catalog/Catalog.API/data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public  CatalogContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var client = new MongoClient(_configuration["DatabaseSettings:ConnectionString"]);
+            var dataBase = client.GetDatabase(_configuration["DatabaseSettings:DatabaseName"]);
+            Products=dataBase.GetCollection<Product>(_configuration["DatabaseSettings:CollectionName"]);
+             CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products  { get; }
+    }
+}
