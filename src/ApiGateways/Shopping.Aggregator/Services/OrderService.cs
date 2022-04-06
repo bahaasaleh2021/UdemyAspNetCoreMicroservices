e7f9477ddab39763450ba0c95dc3297137a27959
Bahaa Saleh <bahaasaleh2021@hotmail.com>
2022-04-06 03:11:49 +0300

    End Of Solution

---
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.HttpClientExtensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly HttpClient _client;
+
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Order/GetOrdersByUserName/{userName}");
+            if(!response.IsSuccessStatusCode)
+                throw new Exception(response.StatusCode.ToString());
+
+            return await response.ReadContentAs<List<OrderResponseModel>>();
+        }
+    }
+}
